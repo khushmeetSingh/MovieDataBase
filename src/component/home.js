feb1a054bd7277b48756757d760000a4de83e75a
khushmeetSingh <skhushmeet7@gmail.com>
2023-09-01 00:05:11 -0400

    another approach to call async by useEffect hook

--- src/component/home.js
+++ src/component/home.js
@@ -1,34 +1,29 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Cards from './card';
 import Hero from './hero'
-const Home=()=>{
-    // const [topMovies,setTopMovies] = useState([]);
-    const homepage = ()=>{
-        let movies =[];
-        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&api_key=857e68ea3b0ca186589c1805e08dacd9')
-        .then(response=>response.json())
-        .then(data=>{
-             movies = data.results;
-        }) 
+const Home= ()=>{
+    const [movies,setMovies] = useState([]);
+    useEffect(()=>{
+        async function homepage(){
+            // let movies =[];
+            await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&api_key=857e68ea3b0ca186589c1805e08dacd9')
+            .then(response=>response.json())
+            .then(data=>{
+                setMovies(data.results)
+        })}
+        homepage();
+    })
 
-        console.log(movies)
-        // const moviejsx = await movies.map((obj)=>{
-        //     return <Cards movie = {obj}/>
-        // })
-        return "Hello";
-    }
-    console.log(typeof(homepage()))
     return(
     <>
         <Hero text="Hello Home"/>
         <div className='container'>
             <div className='row'>
                 <div className='col-lg-8 offset-lg-2 my-2'>
-                    {homepage()}
-                  {/* {homepage().map((obj)=>{
-                       return <Cards movie = {obj}/>
-
-                    })} */}
+                    {movies.map((obj)=>{
+                        return <Cards movie = {obj}/>
+                    })}
                 </div>
             </div>
         </div>
